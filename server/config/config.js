d61fca0a383363f94705543984edf86a39635df0
Hector <hzea@inclusionservices.com>
2019-07-24 10:22:38 -0300

    env var remote mongoose atlas

--- server/config/config.js
+++ server/config/config.js
@@ -7,5 +7,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 //base de datos
 let DatabaseURL;
-DatabaseURL = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/hs_database' : 'mongodb+srv://hectorzea:dtW3YP1cnf0f2Fpe@cluster0-7xou3.mongodb.net/hs_database'
+DatabaseURL = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/hs_database' : process.env.remoteURL
 process.env.DATABASEURL = DatabaseURL;
